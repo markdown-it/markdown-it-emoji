afa6a80c95394573f684363bbaadd2001a015f25
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-12-28 08:13:08 +0300

    Browser files rebuild

--- dist/markdown-it-emoji.js
+++ dist/markdown-it-emoji.js
@@ -1,4 +1,4 @@
-/*! markdown-it-emoji 0.1.2 https://github.com//markdown-it/markdown-it-emoji @license MIT */!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.markdownitEmoji=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*! markdown-it-emoji 0.1.3 https://github.com//markdown-it/markdown-it-emoji @license MIT */!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.markdownitEmoji=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports={
   "100": "💯",
   "911": "🚨",
@@ -1200,7 +1200,8 @@ module.exports = {
   mad:              [ '>:(', '>:-(' ], // angry
   blush:            [ ':")', ':-")' ],
   broken_heart:     [ '</3', '<\\3' ],
-  confused:         [ ':\\', ':-\\', ':/', ':-/' ], // twemoji shows question
+  // :/ & :\ disabled due conflicts, untill logic improved
+  confused:         [ /*':\\',*/ ':-\\', /*':/',*/ ':-/' ], // twemoji shows question
   cry:              [ ":'(", ":'-(", ':,(', ':,-(' ],
   frowning:         [ ':(', ':-(' ],
   heart:            [ '<3' ],
