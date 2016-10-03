1625425a5fba0a5bc13f369e38976beeee221a72
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-10-03 21:49:50 +0300

    Fix upgrader: ignore variation chars in light data filter

--- support/update.js
+++ support/update.js
@@ -39,7 +39,7 @@ request(emojiSrc, function (err, response, body) {
   var visible = fs.readFileSync(p.join(__dirname, 'visible.txt'), 'utf8');
 
   var emoji_light = _.omitBy(emojies, function (val) {
-    return visible.indexOf(val) < 0;
+    return visible.indexOf(val.replace(/\uFE0F/g, '')) < 0;
   });
   fs.writeFileSync(p.join(__dirname, '../lib/data/light.json'), JSON.stringify(emoji_light, null, 2), 'utf8');
 
